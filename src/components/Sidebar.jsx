6664c8259cbddc9e54378e19e8c56b0ff6eae92f
Maite Perez Vento <maitepv87@gmail.com>
2025-05-20 22:55:51 -0400

    add Modal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ export const Sidebar = () => {
     { label: "Dropdown", path: "/" },
     { label: "Accordion", path: "/accordion" },
     { label: "Buttons", path: "/buttons" },
+    { label: "Modal", path: "/modal" },
   ];
 
   const renderedLinks = links.map((link) => {
